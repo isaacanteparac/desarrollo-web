71d4f7a2a26394c2c61cc243920450aef7c2436a
ISAAC ANTEPARA <69361351+isaacanteparac@users.noreply.github.com>
2022-10-03 17:38:42 -0500

    👍

---
+++ src/dom/talleres/taller_2/index.js
@@ -0,0 +1,22 @@
+var count = 1;
+var countColor = 1;
+function addBox() {
+  var contentBox = document.getElementById("contentBox");
+
+  var box = document.createElement("div");
+  box.className = "box";
+
+  if (countColor == 1) {
+    box.className += " colorBox1";
+  } else if (countColor == 2) {
+    box.className += " colorBox2";
+  } else {
+    box.className += " colorBox3";
+    countColor = 0;
+  }
+  countColor += 1;
+  var texto = document.createTextNode(count);
+  box.appendChild(texto);
+  count += 1;
+  contentBox.appendChild(box);
+}
